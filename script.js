371a9b5fc7a4e94118dd6b400188e7330c0c797a
MaksymTovstolis <85698971+MaksymTovstolis@users.noreply.github.com>
2021-09-28 15:43:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 let a = [2, 4, 6, 33,`hello` ,25 ,true];
 let b = [`world`,5,93,false,`kebab`];
-let sumA = 0;
-let sumB = 0;
+// let sumA = 0;
+// let sumB = 0;
 
 
 
 function showMaxValue(a,b) {
-    for (i=0;i < a.length; i++) {
+    let sumB;
+    let sumA;
+    for (let i=0;i < a.length; i++) {
         if (typeof a[i] === 'number') {
            sumA += a[i];
         } 
     }
     
-    for (i=0;i < b.length; i++) {
-        if (typeof b[i] === 'number') {
-           sumB += b[i];
+    for (let j=0;j < b.length; j++) {
+        if (typeof b[j] === 'number') {
+           sumB += b[j];
         } 
     }
-    
+
     if (sumA < sumB) {
         return b;
-    } else {
-        return a;
-    }
+    } 
+    return a;
 }
